924054796c6679dd0df7ab2a8d8c8ee74ecd1985
Genesis Cruz <genesis.cruzasturias@gmail.com>
2023-10-23 16:07:27 -0500

    commit before i restart computer

--- albums_migration.sql
+++ albums_migration.sql
@@ -7,11 +7,11 @@ drop table if exists albums;
 #create album table
 CREATE TABLE albums (
                         id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-                        artist_name VARCHAR(50) DEFAULT 'NONE',
-                        record_name VARCHAR(50) DEFAULT 'NONE',
-                        release_date int(255),
-                        sales DECIMAL(12, 2),
-                        genre CHAR(255) DEFAULT 'NONE',
+                        artist_name VARCHAR(50) NOT NULL,
+                        record_name VARCHAR(50) NOT NULL,
+                        release_date int(255) NOT NULL,
+                        sales DECIMAL(3, 1) NOT NULL,
+                        genre CHAR(255) NOT NULL,
                         PRIMARY KEY (id)
 );
 
