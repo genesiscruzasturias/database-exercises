fd563fc7e94b0b615dcb551f88c76d31ec5c6ca0
Genesis Cruz <genesis.cruzasturias@gmail.com>
2023-10-23 16:35:20 -0500

    commit for next portion of exercises

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,2 +1,14 @@
 use codeup_test_db;
+#Select pink floyd albums
+select * from albums where artist_name = 'pink floyd' or artist_name = 'Pink Floyd';
+#Select release date for beetles song
+select release_date from albums where record_name = 'Sgt. Pepper\'s Lonely Hearts Club Band';
+#Select Genre
+select genre from albums where record_name = 'Dangerous';
+#Select 1990 release date
+select record_name from albums where release_date >= 1990;
+#Select less than 20.0 million sales
+select record_name from albums where sales <= 20.0;
+#Select rock genre
+select record_name from albums where genre = 'Rock';
 
