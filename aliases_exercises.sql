a28b33ac165bb8dc6ea3267e560253afef270041
Genesis Cruz <genesis.cruzasturias@gmail.com>
2023-10-25 20:37:01 -0500

    EASY BREEZY

---
+++ aliases_exercises.sql
@@ -0,0 +1,6 @@
+use employees;
+
+# Return 10 employees in a result set named 'full_name' in the format of 'last name, first name' for each employee.
+SELECT CONCAT(emp_no, ' ', '\-', ' ', first_name, ' ', last_name) AS full_name
+FROM employees
+LIMIT 10;
